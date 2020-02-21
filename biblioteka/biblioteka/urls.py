50bb8ddc816c0fafee92ffda2d86300551aacb45
Alexey Gaskov <alexey_gaskov@rapid7.com>
2020-02-21 15:53:58 +0000

    [develop] Adding Virtual Env and catalog for the app

--- biblioteka/biblioteka/urls.py
+++ biblioteka/biblioteka/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
+# Add URL maps to redirect the base URL to our application
+urlpatterns += [
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
